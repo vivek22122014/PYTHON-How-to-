9a58e141b4ae2016405690ec43650d34d82b6d9c
Sky <vivek22122014@gmail.com>
2020-09-16 04:54:53 +0530

    How to Create Any File Using Python

---
+++ How to Create Any File Using Python/How to Create Any File Using Python.py
@@ -0,0 +1,16 @@
+#importing touch
+import touch
+
+
+
+
+
+#calling touch class from touch
+
+touch.touch("Hello.py")
+touch.touch("Hello.js")
+touch.touch("Hello.html")
+touch.touch("Hello.txt")
+
+
+
