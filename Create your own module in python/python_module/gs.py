ce7af2cecbd095c8e0c4f988e8ac918b4a9be168
Sky <vivek22122014@gmail.com>
2020-09-16 06:13:02 +0530

    Create your own module in python

---
+++ Create your own module in python/python_module/gs.py
@@ -0,0 +1,21 @@
+#import module
+import cal
+
+#call add() method
+print(cal.add(10,20))
+
+#call mul() method
+print(cal.mul(10,20))
+
+#call sub() method
+print(cal.sub(10,20))
+
+#call div() method
+print(cal.div(10,20))
+
+#call mod() method
+print(cal.mod(10,20))
+
+
+
+
