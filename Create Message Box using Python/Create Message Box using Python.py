b11d577f80e896794144897bfeb62baa771af373
Sky <vivek22122014@gmail.com>
2020-10-01 17:10:15 +0530

    Create Message Box using Python

---
+++ Create Message Box using Python/Create Message Box using Python.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+from tkinter import messagebox
+
+main=Tk()
+
+
+#messagebox.showinfo("Information","Do Suscribe now")
+#messagebox.showwarning("Warning","Like the Video")
+#messagebox.showerror("Error","Don't Dislike the video!")
+#messagebox.askquestion("Confirm","Did you suscribed or not yet")
+messagebox.askokcancel("YouTube","Did you Like this video")
+
+main.mainloop()
