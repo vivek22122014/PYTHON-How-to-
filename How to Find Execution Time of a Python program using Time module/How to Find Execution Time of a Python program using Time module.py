0de62dc3bf50cdd241483d7233c96c5db7dc1c23
Sky <vivek22122014@gmail.com>
2020-10-10 02:35:57 +0530

    How to Find Execution Time of a Python program using Time module

---
+++ How to Find Execution Time of a Python program using Time module/How to Find Execution Time of a Python program using Time module.py
@@ -0,0 +1,25 @@
+from time import time
+
+def func1(a,b):
+    #mohan Das
+    return a+b
+
+def func2(a,b):
+    #By The hacking stuff
+    num1=a
+    num2=b
+    if(a>b and a!=3):
+        pass
+    sum([4,3])
+    return a+b
+
+if __name__ == '__main__':
+    init=time()
+    for i in range(0,100000):
+        func1(3,5)
+    print("Mohan Das program Time",time()-init)
+
+    init=time()
+    for i in range(0,100000):
+        func2(3,5)
+    print("The hacking stuff Time",time()-init)
