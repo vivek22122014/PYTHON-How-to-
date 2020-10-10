68115ddc12cb4beebeb5b6b726fa43bc09e182eb
Sky <vivek22122014@gmail.com>
2020-10-10 19:48:24 +0530

    using function

---
+++ Factorial of a number/Factorial of a number using function.py
@@ -0,0 +1,25 @@
+def factorial(number):
+    factorial=1
+
+    if(number<0):
+        print("Error: Factorial of negative number is not defined")
+
+
+    elif(number==0):
+        print(1)
+
+    else:
+        for i in range(1,number+1):
+            factorial=factorial*i
+
+        return factorial
+
+
+
+
+if __name__=='__main__':
+    print("Enter the number")
+    number = int(input())
+
+    
+    print("The factorial of",number,"is",factorial(number))
